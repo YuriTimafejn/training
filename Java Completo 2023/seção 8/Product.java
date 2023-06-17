4277a5f43703b6345365540937e43c20e9f81e97
Timafejn, Yuri <timafejn@gmail.com>
2023-06-17 17:17:58 -0300

    Section 8 finished

---
+++ Java Completo 2023/seção 8/Product.java
@@ -0,0 +1,24 @@
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock () {
+        return this.price * this.quantity;
+    }
+
+    public void add (int newQuantity) {
+        this.quantity += newQuantity;
+    }
+
+    public void remove (int removedQuantity) {
+        this.quantity -= removedQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + "\n" +
+        "Price: $ " + String.format( "%.2f",this.price) + "\n" + "In stock: " + this.quantity + " units\n" +
+                "Total in stock: $ " + String.format( "%.2f",totalValueInStock());
+    }
+}

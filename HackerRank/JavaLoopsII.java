69bcb5fbebf9b0a3c55a2660f745f2bdf1eadda3
Timafejn, Yuri <timafejn@gmail.com>
2023-10-18 14:26:13 -0300

    hotfix: change class name

--- HackerRank/JavaLoopsII.java
+++ HackerRank/JavaLoopsII.java
@@ -1,7 +1,6 @@
 import java.util.*;
-import java.io.*;
 
-class Solution{
+class JavaLoopsII{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
